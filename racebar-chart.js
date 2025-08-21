1e1c870fb585f8e0219d25f1faaf8534f211f240
Danny Ucho <45443922+U-Danny@users.noreply.github.com>
2025-08-21 09:04:17 -0500

    Update racebar-chart.js

--- racebar-chart.js
+++ racebar-chart.js
@@ -80,7 +80,6 @@ export async function renderChart(container, datasetUrl, customOptions = {}) {
         grid: {
           left: '1%', 
           right: '1%',
-          bottom: '2%',
           containLabel: true // Importante para que las etiquetas no se corten
         },
         legend: {
